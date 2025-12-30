80c2aaf1a256203780100e0c71fb8747f983eae2
Narsky7 <narskifun@gmail.com>
2025-12-30 13:04:34 +0100

    working lexer

--- main.c
+++ main.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
+#include <stdlib.h>
 
 char *input;
 int pos;
 
+int parse_expression();
 // token definitions
 
 typedef enum {
@@ -23,6 +25,54 @@ typedef struct{
     int value;
 } Token;
 
+void skip_whitespace(){
+    while(input[pos] != '\0' && isspace(input[pos])){
+        pos++;
+    }
+}
+
+Token current_token;
+
+Token get_next_token(){
+    skip_whitespace();
+    char c = input[pos];
+    Token token;
+
+    if (c == '\n' || c == '\0'){
+        token.type = TOKEN_EOF;
+        return token;
+    }
+    
+    if(isdigit(c)){
+        token.type = TOKEN_INT;
+        token.value = 0;
+
+        while (isdigit(input[pos])){
+            token.value = token.value * 10 + (input[pos] - '0');
+            pos++;
+        }
+        return token;
+    }
+
+    if (c == '+'){token.type = TOKEN_PLUS;pos++;return token;}
+    if (c == '-'){token.type = TOKEN_MINUS;pos++;return token;}
+    if (c == '*'){token.type = TOKEN_MUL;pos++;return token;}
+    if (c == '/'){token.type = TOKEN_DIV;pos++;return token;}
+    if (c == '('){token.type = TOKEN_LPAREN;pos++;return token;}
+    if (c == ')'){token.type = TOKEN_RPAREN;pos++;return token;}
+
+    printf("syntax error, '%c' is not a valid character",c);
+    exit(1);
+}
+
+void eat(TokenType type) {
+    if (current_token.type == type) {
+        current_token = get_next_token();
+    } else {
+        printf("Błąd składni! Oczekiwano typu %d, a otrzymano %d\n", type, current_token.type);
+        exit(1);
+    }
+}
 
 int main(){
 
@@ -41,12 +91,8 @@ int main(){
         }
         //Tokenize (lexer)
         //Parse
-        int equals = parse_expression();
         //Evaluate
         //Print
-        printf("%d \n", equals);
-
     }    
-    // todo : fix whitespace bug + fix multiple numbers addiditon problem
     return 0;
 }
